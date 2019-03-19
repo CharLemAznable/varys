3dd9ed9931c2736194ab66cd9dc092414f9f2742
CharLemAznable <545541819@qq.com>
2019-03-19 09:55:36 +0800

    Fixing.

--- corp_token_proxy.go
+++ corp_token_proxy.go
@@ -1,6 +1,8 @@
 package varys
 
 import (
+    . "github.com/CharLemAznable/gokits"
+    "net/http"
     "net/http/httputil"
     "net/url"
 )
@@ -10,7 +12,11 @@ var wechatCorpProxy *httputil.ReverseProxy
 func wechatCorpProxyInitialize() {
     baseURL, err := url.Parse(wechatCorpProxyURL)
     if err != nil {
-        baseURL, _ = url.Parse("https://api.weixin.qq.com/cgi-bin/")
+        baseURL, _ = url.Parse("https://qyapi.weixin.qq.com/cgi-bin/")
     }
     wechatCorpProxy = httputil.NewSingleHostReverseProxy(baseURL)
+    wechatCorpProxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, e error) {
+        LOG.Info(request.URL)
+        writer.Write([]byte(Json(map[string]string{"error": err.Error()})))
+    }
 }
